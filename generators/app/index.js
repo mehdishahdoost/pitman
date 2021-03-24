ecac9bd6805cffb78ee6ca8d86659ed8cf262369
Mehdi Shahdoost <mhdshahdoost@gmail.com>
2021-03-24 14:43:28 +0430

    add maven-java8

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,10 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const fs = require('fs');
 
 module.exports = class extends Generator {
   prompting() {
-    // Have Yeoman greet the user.
     this.log(
       yosay(
         `Welcome to the  ${chalk.yellow('Pitman')} generator!`
@@ -14,29 +14,52 @@ module.exports = class extends Generator {
 
   this.projectTypes = [
       'maven-java8',
-      'maven-java-junit5-surefire',
+      'maven-java8-junit5-surefire',
     ];
 
     const prompts = [
       {
         type: 'list',
-        name: 'userAnswer',
+        name: 'pTypeAnswer',
         message: `Select project type:`,
         choices: this.projectTypes,
+        default: undefined
+      },
+      {
+        type: 'input',
+        name: 'pNameAnswer',
+        message: `Select project name:`,
         default: undefined
       }
     ];
 
     return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
       this.props = props;
     });
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    switch (this.props.pTypeAnswer) {
+      case 'maven-java8': 
+      [
+        'pom.xml'
+      ].forEach(path => this.fs.copy(
+        this.templatePath('maven-java8'),
+        this.destinationPath(this.props.pNameAnswer)
+      ));
+      this._copyDir(this.props.pNameAnswer + '/src/main/java');
+      this._copyDir(this.props.pNameAnswer + '/src/main/resources');
+      this._copyDir(this.props.pNameAnswer + '/src/test/java');
+    }
   }
+
+  _copyDir(path) {
+    fs.mkdir(path,{ recursive: true }, (err) => {
+      if (err) {
+          throw err;
+      }
+      console.log("Directory is created.");
+  });
+  }
+ 
 };
