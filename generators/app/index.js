4e851fba902027e113b8669b4116d418cca672dd
Mehdi Shahdoost <mhdshahdoost@gmail.com>
2021-03-30 10:21:55 +0430

    add maven-java8-junit4-surefire template

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,7 +7,11 @@ module.exports = class extends Generator {
   prompting() {
     this.log(yosay(`Welcome to the  ${chalk.yellow("Pitman")} generator!`));
 
-    this.projectTypes = ["maven-java8", "maven-java8-junit5-surefire"];
+    this.projectTypes = [
+      "maven-java8",
+      "maven-java8-junit4-surefire",
+      "maven-java8-junit5-surefire"
+    ];
 
     const prompts = [
       {
@@ -45,6 +49,15 @@ module.exports = class extends Generator {
       case "maven-java8-junit5-surefire":
         this.fs.copy(
           this.templatePath("maven-java8-junit5-surefire"),
+          this.destinationPath(this.props.pNameAnswer)
+        );
+        this._copyDir(this.props.pNameAnswer + "/src/main/java");
+        this._copyDir(this.props.pNameAnswer + "/src/main/resources");
+        this._copyDir(this.props.pNameAnswer + "/src/test/java");
+        break;
+      case "maven-java8-junit4-surefire":
+        this.fs.copy(
+          this.templatePath("maven-java8-junit4-surefire"),
           this.destinationPath(this.props.pNameAnswer)
         );
         this._copyDir(this.props.pNameAnswer + "/src/main/java");
